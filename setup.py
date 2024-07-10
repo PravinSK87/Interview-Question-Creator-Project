af808eb7bb999f4c39c7a805a85234b5205418af
PravinSK <pravinsk87@gmail.com>
2024-07-10 06:57:49 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'Interview Q/A Project',
+    version='0.0.0',
+    author='Pravin',
+    author_email='Pravin@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
